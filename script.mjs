f86c5a1e4c62db6be478288fd82de3829de3862b
TruffleBug <lisa.ho1022@gmail.com>
2024-12-10 16:32:53 -0700

    Finished--uses multiway search tree

--- script.mjs
+++ script.mjs
@@ -1,43 +1,14 @@
-// import { Tree } from "./tree.mjs";
-
 function knightMoves(start, end) {
-    // let startChildrenNode = [];
-    // generateMoveList(start).forEach(element => {
-    //     startChildrenNode.push(new createNode(element));
-    // });
-    // let tree = new createNode(start, startChildrenNode);
-    // console.log('ROOT OF TREE', tree);
     
     let tree = new createNode(start)
-    console.log('ROOT OF TREE', tree);
-    let path = [tree.value]
-    let count = levelOrder(callback, tree, end); 
+    let path = levelOrder(tree, end); 
 
-            // if (checkIfContains(possibleMoves, end) === true) {
-            // path.push(` -> [${end}]`);
-            // const count = path.length - 1;
-            // return `You made it in ${count} moves! Here's your path: ${path}`;
-
-    return `You made it in ${count} moves! \nHere's your path: ${path}`
-    // function recursiveCheckForEnd(current) {
-    //     let moveList = generateMoveList(current.value);
-    //     if(checkIfContains(moveList, end) === true) {
-    //         console.log('You made it!');
-    //         return
-    //     } else {
-    //         let newPossibleMoves = current.possibleMoves.map((element) => {
-    //             return new createNode(element, generateMoveList(element));
-    //         });
-    //         current.possibleMoves = newPossibleMoves;
-    //     }
-    // }
-    // recursiveCheckForEnd(tree);
-    // console.log('tree', tree);
+    return `You made it in ${path.length} moves! \nHere's your path: ${path.join(' -> ')}`
 };
 
 // returns array of all possible moves from vertice
-function generateMoveList(vertice) {
-    const possibleMoves = [
+function generateMoveList(vertice, path) {
+    const moveConfig = [
         [-2, -1],
         [-2, 1],
         [-1, -2],
@@ -48,11 +19,26 @@ function generateMoveList(vertice) {
         [2, 1],
     ];
     let adjacencyList = [];
-    possibleMoves.forEach((move) => { 
-        if (vertice[0] + move[0] < 0 || vertice[1] + move[1] < 0) {
+    moveConfig.forEach((element) => { 
+        const move = ([vertice[0] + element[0], vertice[1] + element[1]])
+
+        if (move[0] < 0 || move [1] < 0 || move[0] > 7 || move [1] > 7) {
             return;
-        };
-        adjacencyList.push([vertice[0] + move[0], vertice[1] + move[1]]);
+        } 
+        
+        path.forEach((element) => {
+            if(JSON.stringify(element) == JSON.stringify(move)) {
+                return;
+            }
+        })
+
+        // if (vertice[0] + element[0] < 0 || vertice[1] + element[1] < 0) {
+            //     return;
+            // };
+            
+        adjacencyList.push(move);
+
+        // adjacencyList.push([vertice[0] + element[0], vertice[1] + element[1]]);
     });
     return adjacencyList;
 };
@@ -65,51 +51,29 @@ function createNode(value, possibleMoves = null) {
     }
 };
 
-// checks if moves list array contains end
-function checkIfContains(possibleMoves, end) {
-    const possibleMovesString = JSON.stringify(possibleMoves);
-    const endString = JSON.stringify(end);
-    if(possibleMovesString.indexOf(endString) != -1) return true;
-    return false;
-};
-
-// checks if specific value = end
-function checkForEnd(value, end) {
-    if(JSON.stringify(value) == JSON.stringify(end)) return 'YOU FOUND A PATH'
-};
-
-// calls callback on each node using breadth-first traversal method
-function levelOrder(callback, tree, end) {  
-    let queue = [tree];
-    let count = 1;
+// // calls callback on each node using breadth-first traversal method
+function levelOrder(tree, end) {  
+    let queue = [{ node: tree, path: []}];
 
     while (queue.length > 0) {
-        const value = queue.shift();
-        console.log('value', value.value);
-        count++
-        console.log('count', count)
-        // callback(value, end);
-        if(callback(value, end) === true) return count;
+        const {node, path} = queue.shift();
 
-        queue = queue.concat(value.possibleMoves);
-        // console.log('queue', queue)
-    };
-};
+        if(JSON.stringify(node.value) == JSON.stringify(end)) {
+            return path.concat(`[${node.value}]`);
+        };
 
-function callback(value, end, count) {
-    if(!value.possibleMoves) {
-        if(JSON.stringify(value.value) == JSON.stringify(end)) {
-            return true;
-        } else { 
+        if(!node.possibleMoves) {
             let startChildrenNode = [];
-            generateMoveList(value.value).forEach(element => {
+            generateMoveList(node.value, path).forEach(element => {
                 startChildrenNode.push(new createNode(element));
             });
-            value.possibleMoves = startChildrenNode;
+            node.possibleMoves = startChildrenNode;
+        };
+
+        for(const child of node.possibleMoves) {
+            queue.push({ node: child, path: path.concat(`[${node.value}]`) })
         };
     };
 };
 
-
-console.log(knightMoves([0,0], [0, 2]));
-// console.log(generateMoveList([2,1]))
+console.log(knightMoves([6,5], [5,2]));
